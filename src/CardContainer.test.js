e7cc071ffda392d2a672b38213d11abc2ff8b4a1
Elizabeth Asbell <e.asbell8@gmail.com>
2019-02-23 17:45:16 -0700

    Start to CardContainer tests.

---
+++ src/CardContainer.test.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import CardContainer from './CardContainer';
+import { shallow } from 'enzyme';
+
+const mockData = [
+  {
+    "id": 1,
+    "prompt": "Make a directory",
+    "possibleChoices": ["cdir", "mkdir", "newdir"],
+    "correctAnswer": "mkdir",
+    "save": false
+  },
+  {
+    "id": 2,
+    "prompt": "Change directory",
+    "possibleChoices": ["change", "cngdir", "cd"],
+    "correctAnswer": "cd",
+    "save": false
+  },
+  {
+    "id": 3,
+    "prompt": "Go to previous directory",
+    "possibleChoices": ["cd ..", "cd .", "prev .."],
+    "correctAnswer": "cd ..",
+    "save": false
+  }
+]
+
+describe('CardContainer', () => {
+  const wrapper = shallow(
+    <CardContainer
+      allData={mockData}
+    />
+  );
+
+  it('should match snapshot with all data passed in', () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should have proper values for default states', () => {
+    expect(wrapper.state()).toEqual({guessCorrectly: '', incorrectGuesses: []});
+  });
+
+})
